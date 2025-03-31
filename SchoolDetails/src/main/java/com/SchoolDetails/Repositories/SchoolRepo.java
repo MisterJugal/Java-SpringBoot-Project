e484498e726c5a3c342cf1c7782377fae083fcc5
Jugal <Jugalpandey6@gmail.com>
2025-03-31 11:35:28 +0530

    Cleaning master branch

--- SchoolDetails/src/main/java/com/SchoolDetails/Repositories/SchoolRepo.java
+++
@@ -1,9 +0,0 @@
-package com.SchoolDetails.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SchoolDetails.Entities.School;
-
-public interface SchoolRepo extends JpaRepository<School, Integer>{
-
-}
