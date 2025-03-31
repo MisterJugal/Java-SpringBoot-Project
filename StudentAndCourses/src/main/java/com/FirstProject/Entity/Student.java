d4a00a10ba5c076eb902c2bbb62ab5a40e14a596
Jugal_Laptop <judey5514@gmail.com>
2025-03-31 12:58:47 +0530

    Added getters and setters in Student

--- StudentAndCourses/src/main/java/com/FirstProject/Entity/Student.java
+++ StudentAndCourses/src/main/java/com/FirstProject/Entity/Student.java
@@ -16,11 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@ToString
-@NoArgsConstructor
+
 @Entity
 public class Student {
 	@Id
@@ -45,5 +41,88 @@ public class Student {
 	@jakarta.persistence.Transient
 	private Home home;
 
+	public int getStudentId() {
+		return studentId;
+	}
 
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getStudentLevel() {
+		return studentLevel;
+	}
+
+	public void setStudentLevel(int studentLevel) {
+		this.studentLevel = studentLevel;
+	}
+
+	public List<Course> getCourse() {
+		return course;
+	}
+
+	public void setCourse(List<Course> course) {
+		this.course = course;
+	}
+
+	public int getSchoolId() {
+		return schoolId;
+	}
+
+	public void setSchoolId(int schoolId) {
+		this.schoolId = schoolId;
+	}
+
+	public int getHomeId() {
+		return homeId;
+	}
+
+	public void setHomeId(int homeId) {
+		this.homeId = homeId;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+	public Home getHome() {
+		return home;
+	}
+
+	public void setHome(Home home) {
+		this.home = home;
+	}
+
+	public Student(int studentId, String studentName, int studentLevel, List<Course> course, int schoolId, int homeId,
+			School school, Home home) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentLevel = studentLevel;
+		this.course = course;
+		this.schoolId = schoolId;
+		this.homeId = homeId;
+		this.school = school;
+		this.home = home;
+	}
+
+	public Student() {
+		super();
+	}
+
+
+	
+	
 }
